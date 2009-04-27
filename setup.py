aae081b70a2816622af6d99b53d229af2855c6c5
ulif <ulif@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-04-27 11:14:28 +0000

    Update setup.py: require zc.buildout, add tests requirements, set URL, update description to include doctests.

--- setup.py
+++ setup.py
@@ -6,17 +6,19 @@ version = '1.2'
 setup(name='collective.recipe.template',
       version=version,
       description="Buildout recipe to generate a text file from a template",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.txt").read() + "\n\n" +
+                       open(os.path.join("collective", "recipe", "template",
+                                         "README.txt")).read() + "\n\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
           "Framework :: Buildout",
           "Programming Language :: Python",
           "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
+      keywords='template recipe',
       author='Wichert Akkerman',
       author_email='wichert@wiggy.net',
-      url='',
+      url='http://pypi.python.org/pypi/collective.recipe.template',
       license='BSD',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.recipe' ],
@@ -24,7 +26,12 @@ setup(name='collective.recipe.template',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'zc.buildout',
       ],
+      extras_require=dict(
+        test = ['zope.testing',]
+        ),
+            
       entry_points="""
       [zc.buildout]
       default = collective.recipe.template:Recipe
